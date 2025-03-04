fa27084b968fc756146421ceb755e2e82d1e9641
Vortex <honor.ston3@gmail.com>
2025-03-04 09:45:54 -0500

    Added Guard in Tenant

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -6,7 +6,9 @@ export class AuthenticationController {
   constructor(private readonly AuthenticationService: AuthenticationService) {}
 
   @Post('login')
-  async login(@Body() { email, password }: { email: string; password: string }) {
+  async login(
+    @Body() { email, password }: { email: string; password: string },
+  ) {
     const user = await this.AuthenticationService.validateUser(email, password);
     if (!user) throw new UnauthorizedException();
 
--- src/tenant/tenant.controller.ts
+++ src/tenant/tenant.controller.ts
@@ -1,27 +1,44 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { TenantService } from './tenant.service';
 import { CreateTenantDto } from './dto/create-tenant.dto';
+import { AuthGuard } from 'src/middleware/guards/authentication.guard';
+import { RolesGuard } from 'src/middleware/guards/role.guard';
+import { Roles } from 'src/middleware/guards/role.decorator';
 
 @Controller('tenant')
+@UseGuards(RolesGuard)
+@UseGuards(AuthGuard)
 export class TenantController {
   constructor(private readonly tenantService: TenantService) {}
 
   @Post()
+  @Roles('SUPERADMIN')
   create(@Body() createTenantDto: CreateTenantDto) {
     return this.tenantService.create(createTenantDto);
   }
 
   @Get()
+  @Roles('SUPERADMIN')
   findAll() {
     return this.tenantService.findAll();
   }
 
   @Get(':id')
+  @Roles('SUPERADMIN')
   findOne(@Param('id') id: string) {
     return this.tenantService.findOne(id);
   }
 
   @Delete(':id')
+  @Roles('SUPERADMIN')
   remove(@Param('id') id: string) {
     return this.tenantService.remove(id);
   }
